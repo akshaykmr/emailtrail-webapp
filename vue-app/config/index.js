68b522f2a2fbcaa57d7d029cd9637fb45f2de4a2
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-10-30 00:43:50 +0530

    test frontend on heroku

--- vue-app/config/index.js
+++ vue-app/config/index.js
@@ -31,7 +31,13 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // proxy all requests starting with /api to jsonplaceholder
+      '/api': {
+        target: 'http://localhost:8080/', // Run python local server on 8080
+        changeOrigin: true,
+      },
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
