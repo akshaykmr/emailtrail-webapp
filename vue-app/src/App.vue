899b5966934983d8048691f05e8918a2338940fe
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-11-02 00:48:14 +0530

    lay the foundation

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="analyse-email">
+    <form action="" id ="email-form">
+      <textarea class="glowing-border" id="email-source" rows="15" v-model="email"
+      placeholder="Paste email headers here and then click analyse 🔍">
+      </textarea>
+    </form>
   </div>
 </template>
 
@@ -13,16 +16,35 @@ export default {
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      email: '',
+    };
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+#email-form {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+#email-source {
+  padding: 5px;
+  font-size: 20px;
+  font-family: "Cutive Mono";
+  width: 90%;
+  max-width: 780px;
+  margin: 20px;
+  margin-top: 10px;
+}
+.glowing-border {
+    border: 2px solid #dadada;
+    border-radius: 3px;
+}
+
+.glowing-border:focus {
+    outline: none;
+    border-color: #c4f2eb;
+    box-shadow: 0 0 10px #c4f2eb;
 }
 </style>
