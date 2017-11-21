aaf0e36682a9cd6e894adc2b87747eda2b2d18b8
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-11-22 00:05:33 +0530

    UX improvements

---
+++ vue-app/src/sampleEmail.js
@@ -0,0 +1,25 @@
+const sampleEmail = `Delivered-To: money@capitalism.com
+Received: by 10.129.52.209 with SMTP id b200csp1430876ywa;
+        Tue, 10 Oct 2017 01:17:02 -0700 (PDT)
+X-Received: by 10.31.153.20 with SMTP id b20mr6116862vke.110.1507623422746;
+        Tue, 10 Oct 2017 01:17:02 -0700 (PDT)
+Received: from mail-sor-f65.google.com (mail-sor-f65.google.com. [209.85.220.65])
+        by mx.google.com with SMTPS id b31sor1345013uaa.124.2017.10.10.01.17.02
+        for <money@capitalism.com>
+        (Google Transport Security);
+        Tue, 10 Oct 2017 01:17:02 -0700 (PDT)
+Received-SPF: pass (google.com: domain of bags@test_email.ua.edu designates 209.85.220.65 as permitted sender) client-ip=209.85.220.65;
+X-Received: by 10.176.85.196 with SMTP id w4mr6874179uaa.75.1507623422198; Tue, 10 Oct 2017 01:17:02 -0700 (PDT)
+MIME-Version: 1.0
+Received: by 10.103.79.86 with HTTP; Tue, 10 Oct 2017 01:17:01 -0700 (PDT)
+From: Mr. Money Bags <bags@moneyrules.com>
+Date: Tue, 10 Oct 2017 01:17:01 -0700
+Subject:
+To: money@capitalism.com;
+Content-Type: text/plain; charset="UTF-8"
+Bcc: satan@wallstreet.com
+
+A business opportunity awaits
+`;
+
+export default sampleEmail;
